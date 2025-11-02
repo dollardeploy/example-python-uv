a0cdbc4129fb58080af91b0febc92c1976d32911
Ruslan Gainutdinov <ruslan@wizecore.com>
2025-11-02 18:38:01 +0200

    fix: link to repo

--- main.py
+++ main.py
@@ -20,14 +20,24 @@ def get():
                     padding: 2rem;
                     text-align: center;
                 }
+                a {
+                    color: #0366d6;
+                    text-decoration: none;
+                    font-size: 1.2rem;
+                    font-weight: bold;
+                }
+                a:hover {
+                    text-decoration: underline;
+                }
             """)
         ),
         Body(
             Div(
                 H1("Welcome to the Python Example App!"),
-                P("This is a simple web server written in Python. It serves this static HTML page. You can use this as a starting point for building more complex web applications."),
+                P("This is a simple web server written in Python, with FastHTML and uv. It serves this static HTML page. You can use this as a starting point for building more complex web applications."),
                 H2("Run with DollarDeploy"),
                 P("Deploying and managing this application is easy with DollarDeploy. Connect your server, point DollarDeploy to your code repository, and it handles the build, deployment, and monitoring automatically – no SSH required!"),
+                P(A("View on GitHub", href="https://github.com/dollardeploy/example-python-uv/")),
                 cls="container"
             )
         )
